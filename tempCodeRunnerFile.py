9a2d40505faa1cf67276487417634ebf54b84cf1
Acton Healy <afkhealy@gmail.com>
2025-03-07 14:06:48 -0600

    deleted file

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-            print(f"This is the f0 here: {f0[i]}")
-            print(f"This is the voiced_flag here: {voiced_flag[i]}")
-            print(f"This is the voiced_probs here: {voiced_probs[i]}")
